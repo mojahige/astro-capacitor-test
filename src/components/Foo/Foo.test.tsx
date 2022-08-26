2ffae0bb43cf3fba8ada6db52b8cf8e0527aaca6
mojahige <mojahige@gmail.com>
2022-08-26 22:33:14 +0900

    add test
    see: https://dev.to/lexlohr/testing-solidjs-code-beyond-jest-39p

---
+++ src/components/Foo/Foo.test.tsx
@@ -0,0 +1,19 @@
+import { test } from "uvu";
+import * as assert from "uvu/assert";
+import { screen, render, fireEvent } from "solid-testing-library";
+import { isInDom } from "../../helper/test";
+import { Counter } from "./";
+
+test("changes text on click", async () => {
+  await render(() => <Counter />);
+
+  const addButton = await screen.findByRole("button", { name: "+" });
+
+  assert.ok(isInDom(addButton));
+
+  fireEvent.click(addButton);
+
+  assert.ok(await screen.findByText("1"));
+});
+
+test.run();
